885dddbd86422e986411aa5a91483f4f396393c8
Makhmud Aliev <makhmud.aliev@partners.goodrx.com>
2023-02-22 15:07:25 -0500

    create kotlin rest service

---
+++ src/test/kotlin/kotindb/KotinDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotindb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotinDbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
